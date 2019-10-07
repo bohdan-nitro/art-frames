93bf2de4a75792363c2df1918d8c31b25072890b
bogdan-nitro <bogdantorgons0ky0@g0m0ailcom>
2019-10-07 17:48:32 +0300

    icons

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    //  Header functions
+    var headerWrap = '.header';
+
+    $(document).scroll(function () {
+
+        var topOffset = $(this).scrollTop();
+
+        if (topOffset >= 85) {
+            $(headerWrap).removeClass('white');
+            $(this).on(function () {
+               $(this).fadeIn(500, function () {
+                   $(this).attr('src', 'logo_icon.svg').fadeIn(500);
+                   console.log(headerWrap);
+               });
+            });
+
+        } else {
+            $(headerWrap).addClass('white');
+        }
+
+        $('span').on(function () {
+            $(this).toggleClass('black');
+        });
+
+    });
+
+
+
+    //  SANDBOX
+
+    // for (var mainIteration = 0; mainIteration <= 4; mainIteration++) {
+    //
+    //     console.log('main iteration:', mainIteration);
+    //
+    // }
+
+
+    // var integerVariable = parseInt($(headerWrap).data('variable'));
+    // var stringVariable = 'Hello world!';
+    // var arrayVariable = [1, 2, 3, 4, 'hello!'];
+    //
+    // var productCardsContainer = $('.products-store');
+    // var productCards = $('.products-item');
+    //
+    // for (var i = 0; i < 9; i++) {
+    //
+    //     productCardsContainer.append($('.products-item').get(i).outerHTML);
+    //
+    // }
+
+//    console.log(arrayVariable);
+
+
+});
